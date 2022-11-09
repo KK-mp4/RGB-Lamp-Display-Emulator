aea5f5558749a63a5447b842449447afa25e6903
KK <103208695+KK-mp4@users.noreply.github.com>
2022-11-09 06:28:20 +0300

    Delete .nuxt directory

--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "C:/WIP Projects/RGB-Lamp-Display-Emulator/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
